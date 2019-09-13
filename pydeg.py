4986fac352a5ab4f6c3a2bd0a1b26e0489f23017
Rushabh Patel <ihsur7@gmail.com>
2019-09-13 13:16:20 +1000

    Initial Commit

---
+++ pydeg.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import PIL.ImageOps as imops
+import numpy as np
+import scipy as sp
+import scipy.ndimage as ndimage
+
+#take edt
+#identify pixels on the edge
+#turn them off
+
+#for expansion
+#invert image
+#identify pixels on edge
+#turn them on
