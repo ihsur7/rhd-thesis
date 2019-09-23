fc1f299e192df5989b98776e48e60f4db181ade7
Rushabh Patel <ihsur7@gmail.com>
2019-09-23 16:01:25 +1000

    a

--- pydeg.py
+++ pydeg.py
@@ -3,6 +3,7 @@ import PIL.ImageOps as imops
 import numpy as np
 import scipy as sp
 import scipy.ndimage as ndimage
+from pystruts import ImageImporter
 
 #take edt
 #identify pixels on the edge
@@ -12,3 +13,13 @@ import scipy.ndimage as ndimage
 #invert image
 #identify pixels on edge
 #turn them on
+
+class Data(object):
+    def __init__(self):
+        self.data = {}
+    
+in_dir = '/data/downsample-512/'
+imdata = Data().data
+    
+image = ImageImporter(imdata, in_dir).import_image('0-lx')
+print(image)
