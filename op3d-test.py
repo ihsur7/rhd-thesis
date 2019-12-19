4a485999696dfa440f95ff84e625e991ae3698e0
ihsur <ihsur7@gmail.com>
2019-12-19 11:42:37 +1000

    a

--- op3d-test.py
+++ op3d-test.py
@@ -8,8 +8,6 @@ import PIL.Image as Image
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import os
-import pyassimp
-from pyntcloud import PyntCloud
 import pandas as pd
 
 in_dir = "/data/sample1/25/uct/tiff/" #'/data/sample1/25/model/25.stl' #"/data/sample1/25/uct/tiff/"
@@ -20,8 +18,9 @@ input_dir = pyd.Directory(in_dir).InputDIR()
 # print(len(model.meshes))
 
 class Voxelize:
-    def __init__(self, directory):
+    def __init__(self, directory, booltype = False):
         self.directory = directory
+        self.booltype = booltype
 
     def toNumpy(self):
         res = np.asarray(Image.open(self.directory+os.listdir(self.directory)[0])).shape
@@ -38,7 +37,10 @@ class Voxelize:
         coords = np.empty((len(coord[0]), 3), dtype=np.int64)
         for i in np.arange(len(coord[0])):
             coords[i] = coord[:,i]
-        return coords
+        if self.booltype == False:
+            return coords
+        else:
+            return self.npArray()
 
     def matpropsArray(self):
         # Stores properties of points using an array of same length as coord array. 
@@ -54,9 +56,83 @@ class Voxelize:
         for i in cArray:
             nArray[i[0]][i[1]][i[2]] = True
         return nArray
-    
-a = Voxelize(input_dir).coordArray()
 
+# write class which calculates degradation based on data from voxelise
+
+def delPixel():
+    pass
+    
+def neighbours(x, y, z, res):
+    #take coordinates, calculate surrounding coordinates -> return a list of coordinates
+    #create a list of x coordinates
+    x_range = np.arange(x-1, x+2)
+    y_range = np.arange(y-1, y+2)
+    z_range = np.arange(z-1, z+2)
+    n_list = []
+    for i in x_range:
+        for j in y_range: #check if coordinates are negative or larger than image
+            for k in z_range:
+                if -1 < x <= res[0] and -1 < y <= res[1] and -1 < z <= res[2]:
+                    if (x != i or y != j or z != k) and (0 <= i <= res[0]) and (0 <= j <= res[1]) and (0 <= k <= res[2]):
+                        n_list.append([i, j, k])
+    return n_list
+
+a = Voxelize(input_dir)
+# a.coordArray()
+# print(a.coordArray()[0])
+# arr_shape = Voxelize(input_dir).toNumpy()[1]
+# print(arr_shape)
+# n = neighbours(2,2,2, arr_shape)
+# print(n, len(n))
+
+class PixelClassifier():
+    def __init__(self, coords_array, prop_array):
+        self.coords_array = coords_array
+        self.prop_array = prop_array
+
+    def initClassify(self, chi):
+        #prop_array[n][0] = x_i,j
+        #x_i,j = 0 -> amorphous, x_i,j = 1 -> crystalline,
+        #x_i,j = -1 -> eroded
+        #prop_array[n][1] = s
+        #prop_array[n][2] = molecular weight
+        for i in self.prop_array:
+            crys = self.crystallinity(chi)
+            pstate = self.initpixelState(chi)
+            i[0] = crys[1]
+            i[1] = pstate
+    
+    def crystallinity(self, chi):
+        #crystallinity = probability a pixel will be crystalline
+        bin_prob = np.random.binomial(1, chi)
+        if bin_prob == 1:
+            c = chi
+            x_chi = 1
+        else: 
+            c = 1 - chi
+            x_chi = 0
+        return c, x_chi
+    
+    def initpixelState(self, chi):
+        crys = self.crystallinity(chi)
+        if crys[1] == -1:
+            s = 0
+        elif crys[1] == 1:
+            s = 1
+        elif crys[1] == 0:
+            s = 1
+        else:
+            raise ValueError('Undefined pixel state.')
+        return s
+    
+    def update_pixel(self):
+        pass
+    
+def binomial(n, p, size=None):
+    return np.random.binomial(n, p, size)
+print(binomial(1, 0.3))
+
+'''
 # Pass xyz to Open3D.otd.geometry.PointCloud and visualize
 
 pcd = otd.geometry.PointCloud()
@@ -71,6 +147,7 @@ otd.visualization.draw_geometries([pcd])
 # pcd_load = otd.io.read_point_cloud("sync.ply")
 # otd.visualization.draw_geometries([pcd_load])
 
+'''
 '''
 x = np.linspace(-3, 3, 401)
 mesh_x, mesh_y = np.meshgrid(x, x)
