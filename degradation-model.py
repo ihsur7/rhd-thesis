b0a35c360ef05cdd3a88deb3a44650ab6967c7b9
Rushi <ihsur7@gmail.com>
2021-01-06 16:05:15 +1000

    a

--- degradation-model.py
+++ degradation-model.py
@@ -28,29 +28,43 @@ output_dir = dir.OutputDIR()
 
 new_input_dir = pyd.Directory(out_dir).InputDIR()
 
-print(input_dir)
-print(output_dir)
+# print(input_dir)
+# print(output_dir)
 
 #Fick's Law of Diffusion 
 ##Assume 1D initially, infinite source
 ##Molarity (concentration): C = m/V * 1/MW
 ##m = mass of solute (g), V is volume of solution in (L), MW is molecular weight, C is molar concentration (mol/L)
 
-diff_coeff = {"25": 51.7, "37": 67.6, "50": 165} #x10^(-12) m^2/s
-pha_density = 1.240 #kg/m3
+diff_coeff = {"25": 51.7e-12, "37": 67.6e-12, "50": 165e-12} #x10^(-12) m^2/s
+diff_coeff_mm = {"37": 67.6e-5} #mm^2/s
+# print(diff_coeff_mm)
+pha_density = 1.240 * (1/1000**2) #g/m3
 pixel_scale = 1 #mm/px * 1px
 voxel_vol = 1 #mm^3
 voxel_mass = pha_density * voxel_vol
+water_mass = voxel_mass * 0.00984
+water_conc = water_mass/voxel_vol
 
 #Fick's 2nd Law determines concentration change over time - eq. similar to heat eq
 #x goes from 0 -> 1 going through the length of the voxel
 #iter = time, the function is meant to run each iteration to update the concetration of water in the exposed voxel
-
+#conecntration units: mol/mm^3
+#concentration at surface = M_s = saturation mass of water
+#therefore, Fick function determines 
 
 def Fick(diff, x, t):
-    return (1/(math.sqrt(4*math.pi*diff*t)))*(math.exp(-((x**2)/(4*diff*t))))
+    """
+    returns C/C0
+    C0 = water_conc
+    """
+    return math.erfc(x/(math.sqrt(4*diff*t)))
+    # return (1/(math.sqrt(math.pi*diff*t)))*(math.exp(-((x**2)/(4*diff*t))))
 
-print(Fick(diff_coeff["37"], 1, 1))
+#once C/C0 reaches 0.5, random walk takes place (initially model 1D flow, move water concentration to next pixel)
+
+
+print(Fick(diff_coeff_mm["37"], 1, 1e30))
 
 #Mw loss function (units g/mol/day) Mw loss rate: 900 g/mol/day
 #crystallinity (Poisson distribution)
